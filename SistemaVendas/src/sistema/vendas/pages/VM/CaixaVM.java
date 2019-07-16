5f3c12c70b7de0a1743d31dca213ca89470044c9
Levi Martins <levimmartins@gmail.com>
2019-07-15 21:35:45 -0300

    Página de Controller referente a caixa.zul

--- SistemaVendas/src/sistema/vendas/pages/VM/CaixaVM.java
+++ SistemaVendas/src/sistema/vendas/pages/VM/CaixaVM.java
@@ -7,6 +7,7 @@ import javax.ejb.Init;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
@@ -15,10 +16,14 @@ import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.Window;
 
+import sistema.vendas.server.beans.cesta.Cesta;
+import sistema.vendas.server.beans.comprador.Comprador;
 import sistema.vendas.server.beans.produto.Produto;
 import sistema.vendas.server.beans.produto.ProdutoFacadeBean;
+import sistema.vendas.server.beans.registrovendas.RegistroVendas;
 import sistema.vendas.server.beans.usuario.UsuarioFacadeBean;
 
 
@@ -29,7 +34,15 @@ public class CaixaVM {
 	public static final String GLOBAL_JNDI = "java:global/sistema_vendas_ear/";
 	
 	private Produto produto;
+	private Produto produtoBanco;
+	private Integer quantidade;
+	
+	
+	private Cesta cesta;
+	private RegistroVendas registroVenda;
 	private ProdutoFacadeBean produtoFacadeBean;
+	
+	private Collection<Cesta> produtosAComprarCesta;
 	private Collection<Produto> produtosAdicionadosCaixa;
 	private Collection<Produto> produtosBanco;
 	
@@ -48,6 +61,9 @@ public class CaixaVM {
 	@Init
 	public void init() {
 		produtosAdicionadosCaixa = new ArrayList<Produto>();
+		produtosAComprarCesta = new ArrayList<Cesta>();
+		registroVenda = new RegistroVendas();
+		 
 	}
 
 	@AfterCompose
@@ -64,13 +80,62 @@ public class CaixaVM {
 		
 		produtosBanco = produtoFacadeBean.findAll();
 		
+		if(produtosAdicionadosCaixa == null) {
+			produtosAdicionadosCaixa = new ArrayList<Produto>();
+		}
+		
+		
+		if(produtosAComprarCesta == null) {
+			produtosAComprarCesta = new ArrayList<Cesta>();
+		}
+		
+	}
+	
+	@Command
+	@NotifyChange("*")
+	public void adicionarNoCarrinho() {
+		try {
+			if(quantidade != null) {
+			
+				if((produtoBanco.getQuantidade() - quantidade) < 0 ) {
+					Clients.showNotification("Quantidade inválida!", Clients.NOTIFICATION_TYPE_WARNING, null, null, 2500);
+				}else {
+					cesta = new Cesta();
+					cesta.setProduto(produtoBanco);
+					cesta.setQuantidade(quantidade);
+					cesta.setRegistroVendas(registroVenda);
+					
+					produtosAComprarCesta.add(cesta);
+					
+					 
+					 
+					produtosAdicionadosCaixa.add(produto);
+					
+					Clients.showNotification("Produto Adicionado!", Clients.NOTIFICATION_TYPE_INFO, null, null, 2500);
+					winListagemBanco.setVisible(false);
+				}
+				
+			}
+		}catch(NullPointerException exp) {
+			Clients.showNotification("Insira a quantidade!", Clients.NOTIFICATION_TYPE_WARNING, null, null, 2500);
+			exp.printStackTrace();
+		}
 		
 	}
 	
 	@Command
 	@NotifyChange("*")
 	public void remover() {
-		
+		try {
+			if(cesta !=null) {
+				produtosAComprarCesta.remove(cesta);
+				Clients.showNotification("Produto Removido do carrinho!", Clients.NOTIFICATION_TYPE_INFO, null, null, 2500);
+
+			}
+		}catch(NullPointerException exp) {
+			Clients.showNotification("Selecione um produto!", Clients.NOTIFICATION_TYPE_WARNING, null, null, 2500);
+			exp.printStackTrace();
+		}	
 	}
 	
 	@Command
@@ -102,6 +167,49 @@ public class CaixaVM {
 	public void setProdutosBanco(Collection<Produto> produtosBanco) {
 		this.produtosBanco = produtosBanco;
 	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Produto getProdutoBanco() {
+		return produtoBanco;
+	}
+
+	public void setProdutoBanco(Produto produtoBanco) {
+		this.produtoBanco = produtoBanco;
+	}
+
+	public Cesta getCesta() {
+		return cesta;
+	}
+
+	public void setCesta(Cesta cesta) {
+		this.cesta = cesta;
+	}
+
+	public RegistroVendas getRegistroVenda() {
+		return registroVenda;
+	}
+
+	public void setRegistroVenda(RegistroVendas registroVenda) {
+		this.registroVenda = registroVenda;
+	}
+
+	public Collection<Cesta> getProdutosAComprarCesta() {
+		return produtosAComprarCesta;
+	}
+
+	public void setProdutosAComprarCesta(Collection<Cesta> produtosAComprarCesta) {
+		this.produtosAComprarCesta = produtosAComprarCesta;
+	}
+	
+	
+	
 	
 	
 
